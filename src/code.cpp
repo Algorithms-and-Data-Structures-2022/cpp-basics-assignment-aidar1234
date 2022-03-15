237632dcc036cd71e2fb74d191b12ce9b777aa7e
Aidar <ajdar.gafiat@gmail.com>
2022-03-15 19:27:30 +0300

    Tasks 1-9

--- src/code.cpp
+++ src/code.cpp
@@ -143,14 +143,8 @@ namespace assignment {
       os << "Invalid argument: k\n";
     }
     else {
-      int c = 1;
-      for (int i = 0; i < length; i++) {
-        if(c == k) {
-          os << arr[i] + "\t";
-          c = 1;
-          continue;
-        }
-        c++;
+      for (int i = 0; i < length; i += k) {
+        os << arr[i] << "\t";
       }
     }
   }
